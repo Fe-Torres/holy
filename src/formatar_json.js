3dea9be9428db26cadbccf3412b2ef79d5b20d18
Lucca Costa <lucca0403@gmail.com>
2024-04-09 09:09:57 -0300

    oieews

--- src/formatar_json.js
+++ src/formatar_json.js
@@ -3,58 +3,109 @@ const path = require('path');
 
 function formatarArquivo(inputFile) {
     try {
-        const partes = inputFile.fileName.split('-');
-        const numeroParte = parseInt(partes[0]);
-
-        const nomeLivro = partes.slice(1).join('-').split('.')[0].trim();
-        const sigla = nomeLivro.substring(0, 2).toLowerCase();
-
+        const texto = JSON.parse(inputFile.text).text;
         const versiculosFormatados = [];
-        let capituloAtual = null;
+        let capituloAtual = 0;
+        let numeroVersiculo = 0;
+        let livro = '';
+        let sigla = '';
+        let anotações = [];
 
-        inputFile.content.forEach((item, index) => {
-            const linha = item[0].trim();
-            if (index === 0) {
-                // Se for o primeiro item, é o título do capítulo ou do salmo
-                capituloAtual = isNaN(parseInt(linha[0])) ? null : parseInt(linha[0]);
-            } else {
-                // Se não for o primeiro item, verifica se é um versículo
-                const match = linha.match(/^(\d+)\s(.+)$/);
-                if (match) {
-                    const versiculo = parseInt(match[1]);
-                    const texto = match[2];
-                    versiculosFormatados.push({
-                        parte: numeroParte,
-                        capítulo: capituloAtual,
-                        versículo: versiculo,
-                        texto: texto,
-                        livro: nomeLivro,
-                        sigla: sigla
-                    });
+        const linhas = texto.split('\n');
+
+        linhas.forEach(linha => {
+            let linhaTrimmed = linha.trim();
+            let parte = null;
+
+            // Verificar se a linha contém um número seguido de uma letra minúscula opcional
+            const parteMatch = /^\d+([a-z]) /.exec(linhaTrimmed);
+            if (parteMatch) {
+                parte = parteMatch[1];
+                linhaTrimmed = linhaTrimmed.replace(parteMatch[0], '');
+            }
+
+            if (/^\d+[a-z]? /.test(linhaTrimmed)) {
+                const versiculoMatch = /^\d+[a-z]?/.exec(linhaTrimmed);
+                if (versiculoMatch) {
+                    const numeroVersiculoStr = versiculoMatch[0];
+                    // Extrair apenas o número do versículo
+                    const numeroVersiculoInt = parseInt(numeroVersiculoStr.match(/\d+/)[0]);
+                    if (!isNaN(numeroVersiculoInt)) {
+                        numeroVersiculo = numeroVersiculoInt;
+                        const textoVersiculo = linhaTrimmed.replace(numeroVersiculoStr, '');
+                        // Verificar se o texto contém mais de um versículo
+                        const versiculosSeparados = textoVersiculo.split(/\d+[a-z]?/).filter(Boolean);
+                        versiculosSeparados.forEach((versiculo, index) => {
+                            versiculosFormatados.push({
+                                capítulo: capituloAtual,
+                                versículo: numeroVersiculo + index,
+                                parte: parte, // Adicionar a parte ao objeto do versículo
+                                texto: versiculo.trim(),
+                                livro: livro,
+                                sigla: sigla,
+                                anotações: anotações
+                            });
+                        });
+                        // Incrementar o número do versículo
+                        numeroVersiculo += versiculosSeparados.length - 1;
+                    }
                 }
+            } else if (/^\d+$/.test(linhaTrimmed)) {
+                capituloAtual = parseInt(linhaTrimmed);
+                numeroVersiculo = 0; // Reiniciar a contagem de versículos ao iniciar um novo capítulo
+            } else if (linhaTrimmed !== '') {
+                if (/^[A-ZÀ-Ú]+$/.test(linhaTrimmed)) {
+                    livro = linhaTrimmed;
+                    sigla = linhaTrimmed.substring(0, 3).toUpperCase();
+                    numeroVersiculo = 0; // Reiniciar a contagem de versículos ao encontrar um novo título de livro
+                }
+                // Dividir o texto em versículos separados
+                const versiculosSeparados = linhaTrimmed.split(/\d+[a-z]?/).filter(Boolean);
+                versiculosSeparados.forEach((versiculo, index) => {
+                    versiculosFormatados.push({
+                        capítulo: capituloAtual,
+                        versículo: numeroVersiculo + index + 1, // Incrementar o número do versículo
+                        parte: parte, // Adicionar a parte ao objeto do versículo
+                        texto: versiculo.trim(),
+                        livro: livro,
+                        sigla: sigla,
+                        anotações: anotações
+                    });
+                });
+                // Incrementar o número do versículo
+                numeroVersiculo += versiculosSeparados.length - 1;
             }
         });
 
         return versiculosFormatados;
     } catch (error) {
-        console.error(`Erro ao formatar o arquivo ${inputFile.fileName}: ${error}`);
+        console.error(`Erro ao formatar o arquivo: ${error}`);
         return [];
+    }
+}
+
+function verificarDiretorio(diretorio) {
+    return fs.existsSync(diretorio);
+}
+
+function criarPastaSeNaoExistir(pasta) {
+    if (!verificarDiretorio(pasta)) {
+        fs.mkdirSync(pasta);
     }
 }
 
 function formatarTodosArquivos() {
     try {
-        const arquivos = fs.readdirSync('./books-json');
-        const arquivosJson = arquivos.filter(arquivo => arquivo.endsWith('.json'));
+        const arquivos = fs.readdirSync('./output-files');
+        const arquivosJson = arquivos.filter(arquivo => arquivo.endsWith('.json')); // Filtro para arquivos .json
         const pastaSaida = './books-json-formatted/';
 
-        if (!fs.existsSync(pastaSaida)) {
-            fs.mkdirSync(pastaSaida);
-        }
+        criarPastaSeNaoExistir(pastaSaida);
 
         for (const arquivo of arquivosJson) {
-            const data = require(`./books-json/${arquivo}`);
-            const versiculosFormatados = formatarArquivo(data);
+            const data = fs.readFileSync(`./output-files/${arquivo}`, 'utf8');
+            console.log('Arquivo:', arquivo);
+            const versiculosFormatados = formatarArquivo({ text: data });
             escreverArquivoFormatado(versiculosFormatados, pastaSaida, arquivo);
         }
 
