f75436db4c3711e8a9288621292783d4e19bfffb
Lucca Costa <lucca0403@gmail.com>
2024-04-22 08:53:39 -0300

    oiiiii

--- src/formatar_json.js
+++ src/formatar_json.js
@@ -1,128 +1,106 @@
 const fs = require('fs');
 const path = require('path');
 
-function formatarArquivo(inputFile) {
-    try {
-        const texto = JSON.parse(inputFile.text).text;
-        const versiculosFormatados = [];
-        let capituloAtual = 0;
-        let numeroVersiculo = 0;
-        let livro = '';
-        let sigla = '';
-        let anotações = [];
+const sourceDir = './input-files';
+const destinationDir = './output-files';
 
-        const linhas = texto.split('\n');
-
-        linhas.forEach(linha => {
-            let linhaTrimmed = linha.trim();
-            let parte = null;
-
-            // Verificar se a linha contém um número seguido de uma letra minúscula opcional
-            const parteMatch = /^\d+([a-z]) /.exec(linhaTrimmed);
-            if (parteMatch) {
-                parte = parteMatch[1];
-                linhaTrimmed = linhaTrimmed.replace(parteMatch[0], '');
-            }
-
-            if (/^\d+[a-z]? /.test(linhaTrimmed)) {
-                const versiculoMatch = /^\d+[a-z]?/.exec(linhaTrimmed);
-                if (versiculoMatch) {
-                    const numeroVersiculoStr = versiculoMatch[0];
-                    // Extrair apenas o número do versículo
-                    const numeroVersiculoInt = parseInt(numeroVersiculoStr.match(/\d+/)[0]);
-                    if (!isNaN(numeroVersiculoInt)) {
-                        numeroVersiculo = numeroVersiculoInt;
-                        const textoVersiculo = linhaTrimmed.replace(numeroVersiculoStr, '');
-                        // Verificar se o texto contém mais de um versículo
-                        const versiculosSeparados = textoVersiculo.split(/\d+[a-z]?/).filter(Boolean);
-                        versiculosSeparados.forEach((versiculo, index) => {
-                            versiculosFormatados.push({
-                                capítulo: capituloAtual,
-                                versículo: numeroVersiculo + index,
-                                parte: parte, // Adicionar a parte ao objeto do versículo
-                                texto: versiculo.trim(),
-                                livro: livro,
-                                sigla: sigla,
-                                anotações: anotações
-                            });
-                        });
-                        // Incrementar o número do versículo
-                        numeroVersiculo += versiculosSeparados.length - 1;
-                    }
-                }
-            } else if (/^\d+$/.test(linhaTrimmed)) {
-                capituloAtual = parseInt(linhaTrimmed);
-                numeroVersiculo = 0; // Reiniciar a contagem de versículos ao iniciar um novo capítulo
-            } else if (linhaTrimmed !== '') {
-                if (/^[A-ZÀ-Ú]+$/.test(linhaTrimmed)) {
-                    livro = linhaTrimmed;
-                    sigla = linhaTrimmed.substring(0, 3).toUpperCase();
-                    numeroVersiculo = 0; // Reiniciar a contagem de versículos ao encontrar um novo título de livro
-                }
-                // Dividir o texto em versículos separados
-                const versiculosSeparados = linhaTrimmed.split(/\d+[a-z]?/).filter(Boolean);
-                versiculosSeparados.forEach((versiculo, index) => {
-                    versiculosFormatados.push({
-                        capítulo: capituloAtual,
-                        versículo: numeroVersiculo + index + 1, // Incrementar o número do versículo
-                        parte: parte, // Adicionar a parte ao objeto do versículo
-                        texto: versiculo.trim(),
-                        livro: livro,
-                        sigla: sigla,
-                        anotações: anotações
-                    });
-                });
-                // Incrementar o número do versículo
-                numeroVersiculo += versiculosSeparados.length - 1;
-            }
-        });
-
-        return versiculosFormatados;
-    } catch (error) {
-        console.error(`Erro ao formatar o arquivo: ${error}`);
-        return [];
-    }
+if (!fs.existsSync(destinationDir)) {
+    fs.mkdirSync(destinationDir, { recursive: true });
 }
 
-function verificarDiretorio(diretorio) {
-    return fs.existsSync(diretorio);
-}
-
-function criarPastaSeNaoExistir(pasta) {
-    if (!verificarDiretorio(pasta)) {
-        fs.mkdirSync(pasta);
-    }
-}
-
-function formatarTodosArquivos() {
-    try {
-        const arquivos = fs.readdirSync('./output-files');
-        const arquivosJson = arquivos.filter(arquivo => arquivo.endsWith('.json')); // Filtro para arquivos .json
-        const pastaSaida = './books-json-formatted/';
-
-        criarPastaSeNaoExistir(pastaSaida);
-
-        for (const arquivo of arquivosJson) {
-            const data = fs.readFileSync(`./output-files/${arquivo}`, 'utf8');
-            console.log('Arquivo:', arquivo);
-            const versiculosFormatados = formatarArquivo({ text: data });
-            escreverArquivoFormatado(versiculosFormatados, pastaSaida, arquivo);
+function formatAndMoveJsonFiles(sourceDir, destinationDir) {
+    fs.readdir(sourceDir, (err, files) => {
+        if (err) {
+            console.error('Erro ao ler diretório:', err);
+            return;
         }
 
-    } catch (error) {
-        console.error(`Erro ao ler o diretório: ${error}`);
-    }
+        files.forEach(file => {
+            if (file.endsWith('.json')) {
+                const sourceFile = path.join(sourceDir, file);
+                const destinationFile = path.join(destinationDir, file);
+
+                fs.readFile(sourceFile, 'utf8', (err, data) => {
+                    if (err) {
+                        console.error('Erro ao ler arquivo:', err);
+                        return;
+                    }
+
+                    try {
+                        const jsonData = JSON.parse(data);
+                        const bookArray = [];
+                        
+                        const verses = jsonData.text.split('\n');
+                        let chapterNumber = 0
+                        verses.forEach((verse) => {
+                            verse = verse.trim();
+                            if (verse !== '') {
+
+                                const verseInText = verse.charAt(0)
+                                const verseIntText2 = verse.charAt(1)
+                                const combinedVerse = verseInText + verseIntText2
+                                const verseInt = parseInt(combinedVerse)
+
+                                if (verseInt === 1) {
+                                    chapterNumber++
+                                }
+                                
+                                const bookName1 = file.replace('.json', '')
+                                const bookName2 = bookName1.slice(3)
+
+                                const sigla = bookName2.charAt(0)
+                                const sigla2 = bookName2.charAt(2)
+                                const siglaCombined = sigla + sigla2
+
+                                const regex = /^\d+\s/
+                                const cleanText = verse.replace(regex, '')
+
+                                const regexNotas = /\d{1,3}:\d{1,3}\s+[a-z][A-Z][a-z]/g
+                                const notas = verse.match(regexNotas)
+                                if (notas && notas.length > 0) {
+                                    const chapterNum = notas[0].split(':')[0]
+                                    const verseNum = notas[0].match(/\d{1,3}:(\d{1,3})/)[1]
+
+
+                                    let chapterFound = null
+                                    for (let i = 0; i < bookArray.length; i++) {
+                                        if (bookArray[i].capítulo === parseInt(chapterNum)) {
+                                            chapterFound = bookArray[i].chapterNum
+                                            break
+                                            
+                                        }      
+                                    }
+                                    
+                                }
+
+
+                                bookArray.push({
+                                    capítulo: chapterNumber,
+                                    versículo: verseInt,
+                                    texto: cleanText,
+                                    livro: bookName2, // Nome do arquivo sem a extensão
+                                    sigla: siglaCombined, // Valor padrão para a sigla
+                                    anotações: []
+                                });
+                            }
+                        });
+
+                        const formattedJson = JSON.stringify(bookArray, null, 2);
+                        fs.writeFile(destinationFile, formattedJson, err => {
+                            if (err) {
+                                console.error('Erro ao escrever arquivo:', err);
+                                return;
+                            }
+
+                            console.log(`Arquivo ${file} formatado e movido com sucesso!`);
+                        });
+                    } catch (err) {
+                        console.error('Erro ao analisar JSON:', err);
+                    }
+                });
+            }
+        });
+    });
 }
 
-function escreverArquivoFormatado(versiculos, pastaSaida, inputFile) {
-    const nomeArquivoSaida = inputFile.replace('.json', '_formatado.json');
-    const caminhoCompleto = path.join(pastaSaida, nomeArquivoSaida);
-    try {
-        fs.writeFileSync(caminhoCompleto, JSON.stringify(versiculos, null, 2));
-        console.log(`Os versículos foram formatados e escritos em ${caminhoCompleto}`);
-    } catch (error) {
-        console.error(`Erro ao escrever o arquivo formatado: ${error}`);
-    }
-}
-
-formatarTodosArquivos();
+formatAndMoveJsonFiles(sourceDir, destinationDir);
